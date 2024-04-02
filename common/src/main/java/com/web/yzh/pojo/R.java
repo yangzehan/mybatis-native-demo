e5991bb4ac59d8752f5952a3509dd0216828509e
杨泽翰 <627617031@qq.com>
2024-04-02 15:53:39 +0800

    更换CommonResult类名为R

--- common/src/main/java/com/web/yzh/pojo/R.java
+++ common/src/main/java/com/web/yzh/pojo/R.java
@@ -1,8 +1,8 @@
 package com.web.yzh.pojo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.web.yzh.exception.ErrorCode;
-import com.web.yzh.exception.GlobalErrorCodeConstants;
+import com.web.yzh.exception.enums.ErrorCode;
+import com.web.yzh.exception.enums.GlobalErrorCodeConstants;
 import java.io.Serializable;
 import java.util.Objects;
 import lombok.Data;
@@ -14,7 +14,7 @@ import org.springframework.util.Assert;
  * @param <T> 数据泛型
  */
 @Data
-public class CommonResult<T> implements Serializable {
+public class R<T> implements Serializable {
 
   /** 错误码 */
   private Integer code;
@@ -28,30 +28,30 @@ public class CommonResult<T> implements Serializable {
   /**
    * 将传入的 result 对象，转换成另外一个泛型结果的对象
    *
-   * <p>因为 A 方法返回的 CommonResult 对象，不满足调用其的 B 方法的返回，所以需要进行转换。
+   * <p>因为 A 方法返回的 R 对象，不满足调用其的 B 方法的返回，所以需要进行转换。
    *
    * @param result 传入的 result 对象
    * @param <T> 返回的泛型
-   * @return 新的 CommonResult 对象
+   * @return 新的 R 对象
    */
-  public static <T> CommonResult<T> error(CommonResult<?> result) {
+  public static <T> R<T> error(R<?> result) {
     return error(result.getCode(), result.getMsg());
   }
 
-  public static <T> CommonResult<T> error(Integer code, String message) {
+  public static <T> R<T> error(Integer code, String message) {
     Assert.isTrue(!GlobalErrorCodeConstants.SUCCESS.getCode().equals(code), "code 必须是错误的！");
-    CommonResult<T> result = new CommonResult<>();
+    R<T> result = new R<>();
     result.code = code;
     result.msg = message;
     return result;
   }
 
-  public static <T> CommonResult<T> error(ErrorCode errorCode) {
+  public static <T> R<T> error(ErrorCode errorCode) {
     return error(errorCode.getCode(), errorCode.getMsg());
   }
 
-  public static <T> CommonResult<T> success(T data) {
-    CommonResult<T> result = new CommonResult<>();
+  public static <T> R<T> success(T data) {
+    R<T> result = new R<>();
     result.code = GlobalErrorCodeConstants.SUCCESS.getCode();
     result.data = data;
     result.msg = "";
