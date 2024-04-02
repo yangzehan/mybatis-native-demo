483500bfb3c25362a1da7aefb1f1f7ce048db823
杨泽翰 <627617031@qq.com>
2024-04-02 17:41:43 +0800

    对齐controller

--- admin/src/main/java/com/web/yzh/controller/IndexController.java
+++ admin/src/main/java/com/web/yzh/controller/IndexController.java
@@ -1,26 +1,25 @@
 package com.web.yzh.controller;
 
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.web.yzh.exegesis.lambdaCapturingTypes;
-import com.web.yzh.generator.domain.User;
-import com.web.yzh.generator.mapper.UserMapper;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author 杨泽翰
  */
-@Tag(name = "测试接口")
-@RestController
+@Tag(name = "首页模块")
+@Controller
 @lambdaCapturingTypes
 public class IndexController {
-  @Autowired private UserMapper mapper;
 
-  @GetMapping("/test")
-  public User createUser() {
+  @GetMapping("/")
+  public String index() {
+    return "index";
+  }
 
-    return mapper.selectOne(Wrappers.lambdaQuery(User.class).eq(User::getUsername, "admin"));
+  @GetMapping("/login")
+  public String login() {
+    return "login";
   }
 }
