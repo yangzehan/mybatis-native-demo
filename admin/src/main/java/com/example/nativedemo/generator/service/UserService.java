4ad60e85dff07fd6985db3d7ce15c66d6382f062
杨泽翰 <627617031@qq.com>
2024-04-03 10:06:57 +0800

    补齐mapper

---
+++ admin/src/main/java/com/example/nativedemo/generator/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.nativedemo.generator.service;
+
+import com.example.nativedemo.generator.domain.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 杨泽翰
+* @description 针对表【user】的数据库操作Service
+* @createDate 2024-04-03 10:06:22
+*/
+public interface UserService extends IService<User> {
+
+}
