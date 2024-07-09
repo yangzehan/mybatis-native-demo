d93fe098894f5b9146b22484fc9862c10e27de1f
杨泽翰 <627617031@qq.com>
2024-07-09 19:59:35 +0800

    缓存用户信息

--- admin/src/main/java/com/web/yzh/handler/MyAuthenticationSuccessHandler.java
+++ admin/src/main/java/com/web/yzh/handler/MyAuthenticationSuccessHandler.java
@@ -1,10 +1,14 @@
 package com.web.yzh.handler;
 
+import cn.hutool.cache.impl.TimedCache;
+import cn.hutool.core.util.IdUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -16,11 +20,14 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-
+@Autowired
+TimedCache timedCache;
   @Override
   public void onAuthenticationSuccess(
       HttpServletRequest request, HttpServletResponse response, Authentication authentication)
       throws IOException {
+
+
 
     // 获取用户身份信息
     Object principal = authentication.getPrincipal();
@@ -31,9 +38,14 @@ public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHand
     result.put("message", "登录成功");
     result.put("data", principal);
     ObjectMapper objectMapper = new ObjectMapper();
+    String key = IdUtil.fastSimpleUUID();
+    result.put("token",key);
     // 转换成json字符串
     String json = objectMapper.writeValueAsString(result);
 
+
+
+    timedCache.put(key, json);
     // 返回响应
     response.setContentType("application/json;charset=UTF-8");
     response.getWriter().println(json);
